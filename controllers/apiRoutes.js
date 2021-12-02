c8520b0eb6cd3352041a2512b79ab99b83518f71
LewisChatham <lewis.chatham@outlook.com>
2021-12-02 12:02:11 +0000

    Reverted path

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Workout } = require("../models/Workout");
+const { Workout } = require("../models");
 
 router.get("/workouts", async (req, res) => {
     try {
